efb5548d0ec3d8fc18d4600602a4dab3e75d81f4
Cesar Del Rio <cesardelriodlr@outlook.com>
2023-06-14 16:19:43 -0700

    creating the charts

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,98 @@
+import { Box, useTheme, Typography } from "@mui/material";
+import { Header } from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
+
+export const FAQ = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            accusantium, repudiandae molestias officia explicabo dolores. Quis
+            eaque odio voluptatem consectetur vero beatae porro, quo numquam
+            quia harum vel soluta. Eveniet?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another Important Question
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            accusantium, repudiandae molestias officia explicabo dolores. Quis
+            eaque odio voluptatem consectetur vero beatae porro, quo numquam
+            quia harum vel soluta. Eveniet?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            A Funny Question
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            accusantium, repudiandae molestias officia explicabo dolores. Quis
+            eaque odio voluptatem consectetur vero beatae porro, quo numquam
+            quia harum vel soluta. Eveniet?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            A Cool Question
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            accusantium, repudiandae molestias officia explicabo dolores. Quis
+            eaque odio voluptatem consectetur vero beatae porro, quo numquam
+            quia harum vel soluta. Eveniet?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            accusantium, repudiandae molestias officia explicabo dolores. Quis
+            eaque odio voluptatem consectetur vero beatae porro, quo numquam
+            quia harum vel soluta. Eveniet?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
+};
